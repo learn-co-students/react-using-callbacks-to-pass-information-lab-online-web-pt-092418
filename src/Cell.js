b008826e328e8a6a7bde6fc9bd147f2e84af42b3
github username <github email address>
2019-07-30 02:06:12 -0400

    Done.

--- src/Cell.js
+++ src/Cell.js
@@ -3,15 +3,28 @@ import React, { Component } from 'react';
 export default class Cell extends Component {
   
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       color: this.props.color
     }
   }
+
+  handleClick = () =>{
+    // once we get the info of the selected color from
+    // the passColor prop we change the state of the color
+    // in the cell
+    const newColor = this.props.passColor;
+    this.setState({
+      color: newColor
+    })
+  };
   
   render() {
     return (
-      <div className="cell" style={{backgroundColor: this.state.color}}>
+        // we add an event listener that will wait for a click
+        // on the page and execute the code to activate the color selector
+      <div className="cell" style={{backgroundColor: this.state.color}}
+      onClick={this.handleClick}>
       </div>
     )
   }
--- src/ColorSelector.js
+++ src/ColorSelector.js
@@ -7,10 +7,20 @@ export default class ColorSelector extends Component {
       return <div key={idx} className="color-swatch" style={{backgroundColor: str}}/>
     })
   )
-  
+
+  handleClick = (e) => {
+    // the event listener for a click is activated we use target to reach the element clicked
+    // using the target to enter into the clicked on element, find the
+    // style it has and access the background color of the selected swatch
+    // we create a variable will will pass on to the clicked method in Matrix
+    const color = e.target.style.backgroundColor;
+    this.props.clickedColor(color)
+  }
+
   render() {
     return (
-      <div id="colorSelector">
+        // Added an onClick to wait for a click in the Swatches
+      <div id="colorSelector" onClick={this.handleClick}>
         {this.makeColorSwatches()}
       </div>
     )
